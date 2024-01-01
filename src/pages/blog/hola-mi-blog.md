45727c5ca45204808947d4f69484340e54835857
parangodev <pabloarangobte@gmail.com>
2024-01-01 10:40:16 -0600

    Update layouts and package.json

--- src/pages/blog/hola-mi-blog.md
+++ src/pages/blog/hola-mi-blog.md
@@ -1,5 +1,5 @@
 ---
-layout: ../../layouts/layout.astro
+layout: ../../layouts/layoutPost.astro
 pubDate: 2021-10-13
 title: "Creando mi primer proyecto en Astro"
 description: "Astro es un nuevo framework para crear sitios web. En este artículo te enseño a crear tu primer proyecto en Astro."
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			textColor:theme => ({
+				primary: '#0F0F0F',
+				secondary: '#232D3F',
+				tertiary: '#005B41',
+			}),
+		},
 	},
 	plugins: [],
 }
